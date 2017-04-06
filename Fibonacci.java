446a49eee78107bfaf3ef8bde809926824c39dce
intan6706150137 <intankhair02@gmail.com>
2017-04-06 08:34:29 +0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Fibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n, a, b, c;
+        Scanner sc = new Scanner(System.in);
+        a=0;
+        b=1;
+        
+        System.out.print("Input : ");
+        n = sc.nextInt();
+        
+        System.out.print(b+" ");
+        for(int i=1; i<n; i++){
+            c=a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+        
+    }
+
+    
+}
